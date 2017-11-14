d11fd3f409c9b1886101ad02bafd6d759a263e2f
Alex Ramsayer <alex.ramsayer@gmail.com>
2017-11-13 19:46:46 -0600

    initial ChocAn eclipse commit

---
+++ src/src/control/ChocAnControl.java
@@ -0,0 +1,18 @@
+package control;
+
+import java.util.ArrayList;
+
+import objects.Member;
+
+public class ChocAnControl {
+	
+	ArrayList<Member> members = new ArrayList<Member>();
+	//finish variables
+	
+	
+	public ProviderDirectory getProviderDirectory(){
+		
+		
+	}
+
+}
