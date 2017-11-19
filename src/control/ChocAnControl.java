57c03242296e7e75a4afbafeb8dea66a401bba54
Alex Ramsayer <alex.ramsayer@gmail.com>
2017-11-19 16:18:25 -0600

    Auto stash before merge of "master" and "origin/master"
    created main, finished ChocAnControl

--- src/control/ChocAnControl.java
+++ src/control/ChocAnControl.java
@@ -8,11 +8,34 @@ import objects.ProviderDirectory;
 public class ChocAnControl {
 	
 	ArrayList<Member> members = new ArrayList<Member>();
-	//finish variables
-	
-	
+	ArrayList<Provider> providers = new ArrayList<Provider>();
+	ArrayList<Visit> visits = new ArrayList<Visit>();
+	ProviderDirectory providerDirectory;
+	AccountingProcedure mainProcedure;	
+	ServiceControl serviceControl;
+	UserInterface userInterface;
+	boolean isFridayAtMidnight;
+
 	public ProviderDirectory getProviderDirectory(){
 		return new ProviderDirectory();
+	}
+	
+	public void runMainAccountingProcedure() {
+	    mainProcedure.produceMemberReports();
+	    mainProcedure.produceManagerSummary();
+	    mainProcedure.produceProviderReports();
+	    mainProcedure.produceEFT();
+	}
+	
+	public void checkTimeAndDate() {
+	  //checks date
+	}
+	
+	public boolean generateReport(String type) {
+	    if(type == "member") mainProcedure.produceMemberReports();
+	    if(type == "manager") mainProcedure.produceManagerSummary();
+	    if(type == "provider") mainProcedure.produceProviderReports();
+	    if(type == "EFT") mainProcedure.produceEFT();
 	}
 
 }
