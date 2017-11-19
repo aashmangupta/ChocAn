5602ff631d42046bbd2d9887d0be46f4c70c0014
dthastings <dthastings@crimson.ua.edu>
2017-11-19 15:58:09 -0600

    Created UserManager.java with method skeleton

---
+++ src/control/UserManager.java
@@ -0,0 +1,30 @@
+package control;
+
+//import java.util.ArrayList;
+
+import objects.Member;
+import objects.Provider;
+
+public class UserManager {
+		
+	public boolean addMember() {
+		return true;
+	}
+	public boolean editMember() {
+		return true;
+	}
+	public boolean deleteMember() {
+		return true;
+	}
+	
+	public boolean addProvider() {
+		return true;
+	}
+	public boolean editProvider() {
+		return true;
+	}
+	public boolean deleteProvider() {
+		return true;
+	}
+	
+}
