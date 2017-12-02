c1e9d367bdc19f421c05eeeb6fa0cc9955fb160e
bosanders <bosanders@crimson.ua.edu>
2017-12-02 14:14:18 -0600

    Edits to ServiceControl and ProviderDirecotry

--- src/control/ServiceControl.java
+++ src/control/ServiceControl.java
@@ -19,6 +19,8 @@ public class ServiceControl {
 	private Provider pro = new Provider();
 	int i = 0;
 	
+	public ServiceControl() {
+	}
 	
 	public ServiceControl(long c, Date d, String com, UserInterface userI) {
 		code = c;
--- src/objects/ProviderDirectory.java
+++ src/objects/ProviderDirectory.java
@@ -2,28 +2,52 @@ package objects;
 
 import objects.Service;
 import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ProviderDirectory {
-	//private Service service = new Service();
 	private ArrayList<Service> services = new ArrayList<Service>();	//makes service ArrayList
+	File writeFile;	//makes file to be written to and sent as an email
 	
 	public ProviderDirectory() {
-		services.add(new Service("Dietitian Session", 598470, 30));			//populates ArrayList
 		services.add(new Service("Aerobics Exercise Session", 883948, 20));	//populates ArrayList
-		services.add(new Service("Shock Therapy Session", 883949, 60));		//populates ArrayList
-		services.add(new Service("Hypnosis Session", 883950, 80));			//populates ArrayList
 		services.add(new Service("Cardio Exercse Session", 883951, 20));		//populates ArrayList
+		services.add(new Service("Dietitian Session", 598470, 30));			//populates ArrayList
+		services.add(new Service("Hypnosis Session", 883950, 80));			//populates ArrayList
+		services.add(new Service("Shock Therapy Session", 883949, 60));		//populates ArrayList
 		services.add(new Service("Error: invalid code", 000000, 0));			//populates ArrayList
 	}
 	
 	/**
+	 * This function writes all the services to a file and emails it to the provider.
 	 * 
-	 * 
-	 * @param
+	 * @param none
 	 * @return void
 	 */
 	public void email() {
+		int i = 0;
+		writeFile = new File("ProviderDirectory.txt");			//creates file
+		Service currServ = new Service();						//holder service
 		
+		try {
+			FileWriter fileWriter = new FileWriter(writeFile);	//sets up file writer
+			fileWriter.write("Services: \n");
+			fileWriter.write("name - code - fee \n");
+			fileWriter.write("");
+			
+			for(i = 0; i < services.size() - 1; ++i) {			//loops through services to write each to file
+				currServ = services.get(i);						//gets a specific service
+				fileWriter.write(currServ.getName() + " - ");	//writes name
+				fileWriter.write(currServ.getCode() + " - ");	//writes code
+				fileWriter.write(currServ.getFee() + " \n");		//writes fee
+			}
+			fileWriter.close();									//closes file
+		}
+		
+		catch(IOException e) {									//catches IOException
+			System.out.println("Error");
+		}
 	}
 	
 	/**
@@ -33,8 +57,8 @@ public class ProviderDirectory {
 	 * @param code
 	 * @return Service
 	 */
-	int i = 0;
 	public Service getService(long code) {
+		int i = 0;
 		for(i = 0; i < 5; ++i) {						//loops through service directory
 			if(services.get(i).getCode() == code) {	//if service code is the code being searched for
 				return services.get(i);				//return service
