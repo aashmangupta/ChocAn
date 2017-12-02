a7cbec7d81c425f6a7903c29418dd2fe380fe10c
Unknown <spncrf@gmail.com>
2017-12-02 14:50:58 -0600

    Operator Interface done

--- src/interfaces/OperatorInterface.java
+++ src/interfaces/OperatorInterface.java
@@ -29,7 +29,7 @@ public class OperatorInterface extends UserInterface {
     	  success = true;
     	  break;
       }
-      }
+    }
     if (success == true){
       UserInterface.prompt("Operator Login Successful");
       logged_in = true;
@@ -39,35 +39,95 @@ public class OperatorInterface extends UserInterface {
       UserInterface.prompt("Incorrect Operator Login");
       return login();
     }
+	}
   }
 
-  public void addMember(){
+  public static void addMember(){
 	  UserManager usermanager = new UserManager();
       usermanager.addMember();
   }
-  public void editMember(){
+  public static void editMember(){
 	  UserManager usermanager = new UserManager();
       usermanager.editMember();
   }
-  public void deleteMember(){
+  public static void deleteMember(){
 	  UserManager usermanager = new UserManager();
 	  usermanager.deleteMember();
   }
-  public void addProvider(){
+  public static void addProvider(){
 	  UserManager usermanager = new UserManager();
 	  usermanager.addProvider();
   }
-  public void editProvider(){
+  public static void editProvider(){
     UserManager usermanager = new UserManager();
     usermanager.editProvider();
   }
-  public void deleteProvider(){
+  public static void deleteProvider(){
 	UserManager usermanager = new UserManager();
     usermanager.deleteProvider();
   }
 
-public static String operatorMenu() {
-	// TODO Auto-generated method stub
-	return null;
+  public static String operatorMenu() {
+	
+	boolean menuActive = true;
+	
+	while(menuActive){
+		UserInterface.prompt("\n\n");
+		UserInterface.prompt("**************************");
+		UserInterface.prompt("** Choc-An Operator Menu **");
+		UserInterface.prompt("**************************");
+		UserInterface.prompt("");
+		UserInterface.prompt("Enter a selection:");
+		UserInterface.prompt("Add Member - \"addm\"");
+		UserInterface.prompt("Edit Member - \"editm\"");
+		UserInterface.prompt("Delete Member - \"deletem\"");
+		UserInterface.prompt("Add Provider - \"addp\"");
+		UserInterface.prompt("Edit Provider - \"editp\"");
+		UserInterface.prompt("Delete Provider - \"deletep\"");
+		UserInterface.prompt("Log Out and return to main menu - \"logout\"")
+		input = scan.next();
+		switch(input){
+		case "addm":
+			UserInterface.prompt("Adding member...");
+			addMember();
+			break;
+	
+		case "editm":
+			UserInterface.prompt("Editing member...");
+			editMember();
+			break;
+	
+		case "deletem":
+			UserInterface.prompt("Deleting member...");
+			deleteMember();
+			break;
+	
+		case "addprovider":
+			UserInterface.prompt("Adding provider...");
+			addProvider();
+			break;
+	
+		case "editprovider":
+			UserInterface.prompt("Editing provider...");
+			editProvider();
+			break;
+		
+		case "deleteprovider":
+			UserInterface.prompt("Delete provider...");
+			editProvider();
+			break;
+	
+		case "logout":
+			menuActive = false;
+			break;
+	
+		default:
+			UserInterface.prompt("Invalid selection, try again");
+			break;
+		}
+	}
+	return "login";
+	
 }
+  
 }
