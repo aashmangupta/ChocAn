822603a58004120733650ee5fcc7e70ffb711425
Aashman <agupta20@crimson.ua.edu>
2017-12-03 17:25:08 -0600

    summary

--- src/objects/SummaryReport.java
+++ src/objects/SummaryReport.java
@@ -1,16 +1,36 @@
 package objects;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class SummaryReport {
-	void email() {
-		
+	File currFile;
+	ArrayList<Visit> visitArray;
+	ArrayList<Provider> providerArray;
+	String dateVisit;
+	
+	public SummaryReport(ArrayList<Visit> visits, ArrayList<Provider> providers) {
+		currFile = new File("summaryReport.txt");
+		visitArray = visits;
+		providerArray = providers;
+		if(visits.isEmpty()) {
+			System.out.println("ERROR: No visits exist so far.");
+			return;
+		}
+		generateFile();
+		return;
 	}
 	
 	void generateFile() {
+		int currProvider;
+		
+		for(int i = 0; i < providerArray.size(); i++) {
+	
+		}
+	}
+	
+	void email() {
 		
 	}
 }
