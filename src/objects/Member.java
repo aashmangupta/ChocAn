513520144288fadb3442713516ab4fdedbc8a96a
dthastings <dthastings@crimson.ua.edu>
2017-12-06 14:36:55 -0600

    Added javadoc to main object classes

--- src/objects/Member.java
+++ src/objects/Member.java
@@ -6,11 +6,25 @@ public class Member extends User{
 	
 	public boolean accountStatus;
 	
+	/**
+	   * Constructor.
+	   */
 	public Member() {
 		super("John Doe", 123456789, "001 main street", "town", "000001", "state");
 		this.accountStatus = true;
 	}
 	
+	/**
+	   * Constructor.
+	   * 
+	   * @param name, name of the member
+	   * @param ID, ID of the member
+	   * @param add, address of the member
+	   * @param city, city of the member
+	   * @param zip, zip code string of the member
+	   * @param st, state of the member
+	   * @param status, boolean to represent the account status of the member 
+	   */
 	public Member(String name, long ID, String add, String city, String zip, String st, 
 			boolean status) {
 		super(name, ID, add, city, zip, st);
--- src/objects/User.java
+++ src/objects/User.java
@@ -11,6 +11,15 @@ public class User {
 	public String zipCode;
 	public String state;
 	
+	/**
+	   * Constructor.
+	   * 
+	   * @param name, name of the user
+	   * @param ID, ID of the user
+	   * @param add, address of the user
+	   * @param zip, zip code string of the user
+	   * @param st, state of the user
+	   */
 	public User(String name, long ID, String add, String city, String zip, String st) {
 		this.name = name;
 		this.id = ID;
