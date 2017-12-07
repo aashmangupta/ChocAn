b8f118751180bebb0b3b47ec022ae71d9855cbc4
johngalt <dthastings@crimson.ua.edu>
2017-12-06 19:49:33 -0600

    Added Javadoc to UserInterface

--- src/interfaces/UserInterface.java
+++ src/interfaces/UserInterface.java
@@ -5,9 +5,22 @@ public abstract class UserInterface {
 
   public abstract boolean login();
 
+  /**
+   * This is the universal print function for ChocAn.
+   * 
+   * @param prompt, string pulled in to print out to console
+   * @return void
+   */
   public static void prompt(String prompt){
       System.out.println(prompt);
   }
+  
+  /**
+   * This is the function initiated to print out the state
+   * 
+   * @param state, string pulled in to print out
+   * @return void
+   */
   public static void state(String state){
 	  System.out.print(state);
   }
