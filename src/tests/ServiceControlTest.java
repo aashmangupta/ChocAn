52d89b5490ffb1f4efabf9c7eef24aa689f0db2d
bosanders <bosanders@crimson.ua.edu>
2017-12-07 13:13:21 -0600

    edited ServiceControl test

--- src/tests/ServiceControlTest.java
+++ src/tests/ServiceControlTest.java
@@ -9,11 +9,21 @@ import control.ServiceControl;
 import objects.Member;
 import objects.Provider;
 import objects.Service;
+import objects.ProviderDirectory;
 
+import java.util.ArrayList;
+import objects.Visit;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import objects.EFTReport;
+
+//Created by Ben Sanders, testing Alex Ramsayer's Method
 
 public class ServiceControlTest {
 	ChocAnControl chocAn;
 	ServiceControl servCont;
+	ProviderDirectory proDir;
 	Member mem;
 	Provider pro;
 	Service serv;
@@ -24,34 +34,51 @@ public class ServiceControlTest {
 
 	@Before
 	public void setUp() throws Exception {
-		chocAn = new ChocAnControl();
-		servCont = new ServiceControl();
-		serv = new Service("test", 900000009, 10);
-		mem = new Member(String name, long ID, String add, String city, String zip, String st, 
-				boolean status);
-		pro = new Provider(String name, long ID, String add, String city, String zip, String st, 
-				int fees, int consul);
 		com = "";
 		tester = true;
 	}
 
 	@Test
 	public void testCreateVisit() {
-		tester = servCont.createVisit(700000001, 800000001, serv, com);
+		servCont = new ServiceControl();
+		proDir = new ProviderDirectory();
+		serv = proDir.getService(883948);
+		mem = new Member("test_member", 1133, "123 street", "northlake", "767676", "Oaklahoma", true);
+		pro = new Provider("test_provider", 3311, "123 lane", "south", "132413", "New Mexico", 10, 0);
+		ChocAnControl.members.add(mem);
+		ChocAnControl.providers.add(pro);
+		
+		
+		tester = servCont.createVisit(1133, 3311, serv, com);
 		assertTrue(tester);
 	}
 	
-	@Test
-	public void testSanity() {
-		tester = servCont.createVisit(mem.id, pro.id, serv, com);
-		tester2 = servCont.createVisit(mem.id, pro.id, serv, com);
+	@Test (expected = NumberFormatException.class)
+	public void testException() {
+		chocAn = new ChocAnControl();
+		servCont = new ServiceControl();
+		serv = proDir.getService(883948);
+		mem = new Member("test_member", 1133, "123 street", "northlake", "767676", "Oaklahoma", true);
+		pro = new Provider("test_provider", 3311, "123 lane", "south", "132413", "New Mexico", 10, 0);
+		ChocAnControl.members.add(mem);
+		ChocAnControl.providers.add(pro);
 		
-		assertEquals(ChocAnControl.visits.get(0), ChocAnControl.visits.get(1));
+		
+		tester = servCont.createVisit(mem.id, pro.id, serv, com);
 	}
 	
 	@Test
 	public void testFailure() {
+		servCont = new ServiceControl();
+		proDir = new ProviderDirectory();
+		serv = proDir.getService(883948);
+		mem = new Member("test_member", 1133, "123 street", "northlake", "767676", "Oaklahoma", false);
+		pro = new Provider("test_provider", 3311, "123 lane", "south", "132413", "New Mexico", 10, 0);
+		ChocAnControl.members.add(mem);
+		ChocAnControl.providers.add(pro);
+		
+		
 		tester = servCont.createVisit(mem.id, pro.id, serv, com);
-		assertEquals(tester, false);
+		assertFalse(tester);
 	}
 }
